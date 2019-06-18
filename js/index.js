3df1357bd42ede786a45aa40dfb7c988910f8a86
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-06-18 11:59:04 -0700

    Initial pull

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//Creates a text alert when user clicks on Fun Bus
+let funBusLogo = document.querySelector(".logo-heading h1");
+funBusLogo.addEventListener("click", event => {
+    alert("This is one fun bus!");
+})
+
+
+//Create hover over for nav items
+let navItems = document.querySelector("nav");
+
+
