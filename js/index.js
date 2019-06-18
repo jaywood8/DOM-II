7ecd99a1ea3f4a9beae335001b5546d6a30c675b
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-06-18 13:49:19 -0700

    Header, nav, and img event listeners added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 // Your code goes here
 
 //Creates a text alert when user clicks on Fun Bus
-let funBusLogo = document.querySelector(".logo-heading h1");
+let funBusLogo = document.querySelector(".logo-heading");
 funBusLogo.addEventListener("click", event => {
+    ;
     alert("This is one fun bus!");
+})
+
+//Highlight Fun Bus when hovered over
+funBusLogo.addEventListener("mouseenter", event => {
+    event.target.style.backgroundColor = "yellow";
+})
+funBusLogo.addEventListener("mouseleave", event => {
+    event.target.style.backgroundColor = "white";
 })
 
 
 //Create hover over for nav items
-let navItems = document.querySelector("nav");
+const navItems = document.querySelector(".nav");
+navItems.addEventListener("mouseenter", event => {
+    event.target.style.backgroundColor = "#D3D3D3";
+})
+navItems.addEventListener("mouseleave", event => {
+    event.target.style.backgroundColor = "white";
+})
+
+//Change main img on click
+let busImage = document.querySelector(".intro img")
+busImage.addEventListener('click', ev => {
+    if(document.getElementById("main-img").src === "http://127.0.0.1:5500/img/fun-bus.jpg") {
+        document.getElementById("main-img").src = "http://127.0.0.1:5500/img/fun-bus-3.jpg";
+    } else if(document.getElementById("main-img").src === "http://127.0.0.1:5500/img/fun-bus-3.jpg") {
+        document.getElementById("main-img").src = "http://127.0.0.1:5500/img/fun-bus.jpg";
+    };
+})
 
 
