6a7ea3e044f5ea9f2f7e904ae26c351ab1eee9ef
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-06-18 15:48:18 -0700

    8 of 9 event listeners added, committing for airtable

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ funBusLogo.addEventListener("mouseleave", event => {
 })
 
 
-//Create hover over for nav items
+//Create hover over for nav bar
 const navItems = document.querySelector(".nav");
 navItems.addEventListener("mouseenter", event => {
     event.target.style.backgroundColor = "#D3D3D3";
@@ -24,6 +24,19 @@ navItems.addEventListener("mouseenter", event => {
 navItems.addEventListener("mouseleave", event => {
     event.target.style.backgroundColor = "white";
 })
+
+
+//Create hover over for each nav item with forEach with a 500ms timeout
+const navLinks = document.querySelectorAll(".nav a");
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', event => {
+        event.target.style.border = "2px solid red";
+        setTimeout(function() {
+            event.target.style.border = "";
+          }, 500);
+    }) 
+})
+
 
 //Change main img on click
 let busImage = document.querySelector(".intro img")
@@ -35,4 +48,55 @@ busImage.addEventListener('click', ev => {
     };
 })
 
+//2 Steps: add random color border to bottom picture w/ random color generator and add event listener
+//1. generate random color (from stack overflow)
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//2. apply random colored border to image upon dblclick 
+let destImg = document.querySelector(".content-destination img");
+destImg.addEventListener("dblclick", event => {
+    event.target.style.border = `5px solid ${getRandomColor()}`;
+})
+
+//2 Steps: generate error and add EL for error 
+//1. breaks img src link upon single click
+let badLink = "notgonnawork"
+let failedImg = document.querySelector(".img-content img")
+failedImg.addEventListener("click", e => {
+    document.querySelector(".img-content img").src = badLink;    
+})
+
+//2. generate error if Let's Go image fail to load along with alert text
+failedImg.addEventListener('error', evt => {
+    console.log('img failure');
+    alert("You ruined our trip!")
+})
+
+//alert whenever intro p text is copied
+let paraText = document.querySelectorAll(".intro p")
+paraText.forEach(text => {
+    text.addEventListener('copy', ev => {
+    alert("Thief! Plagiarist!")
+    })
+})
+
+//alert on keydown on main img
+window.addEventListener("keydown", e => {
+    alert("Key down");
+})
+
+//change color on right click
+let welcomeHeader = document.getElementsByClassName(".text-content h2")
+console.log(welcomeHeader);
+
+welcomeHeader.addEventListener('auxclick', evt => {
+    event.target.style.color = "red";
+})
 
